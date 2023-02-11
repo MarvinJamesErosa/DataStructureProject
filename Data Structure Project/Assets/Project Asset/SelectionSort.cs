9af7b128e5fadb4ebf9e648a7be541e5dbc64743
PresentMoon <104261152+PresentMoon@users.noreply.github.com>
2023-02-11 14:11:02 +0800

    Add files via upload

--- Data Structure Project/Assets/Project Asset/SelectionSort.cs
+++ Data Structure Project/Assets/Project Asset/SelectionSort.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class SelectionSort : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    static void Main(string[] args)
     {
-        
+        int[] a = new int[] { 10, 6, 8, 4, 2 };
+        SelectionSort(a);
+        for (int i = 0; i < a.Length; i++)
+        {
+            Console.Write($"{a[i]} ");
+        }
+        Console.ReadLine();
     }
 
-    // Update is called once per frame
-    void Update()
+    private static void SelectionSort(int[] a)
     {
-        
+        int index = 0, minPos = 0, count = a.Length;
+        while (index < count)
+        {
+            minPos = index;
+            for (int i = index + 1; i < count; i++)
+            {
+                if (a[i] < a[minPos])
+                {
+                    minPos = i;
+                }
+            }
+            int t = a[index];
+            a[index] = a[minPos];
+            a[minPos] = t;
+            index++;
+        }
     }
 }
